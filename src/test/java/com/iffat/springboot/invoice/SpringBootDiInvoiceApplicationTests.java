1fdcb0637e6b1dfb127780acc5f6ed5a778bbe1c
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-22 07:40:19 +0700

    01. Starter Project

---
+++ src/test/java/com/iffat/springboot/invoice/SpringBootDiInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iffat.springboot.invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiInvoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
